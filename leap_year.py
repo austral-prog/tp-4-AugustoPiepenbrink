8c245b57902c0583980ba49c9a0f1c75d1e5bbc4
AugustoPiepenbrink <apiepenbrink@mail.austral.edu.ar>
2025-04-11 12:30:44 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,7 @@
 def leap_year():
-    print("TO DO")
+        txt=int(input("Ingrese un año:"))
+    print(txt)
+    if txt % 4 == 0 and txt % 100!= 0 or (txt % 400 == 0):
+        print(f"El año {txt} es bisiesto")
+    else:
+        print(f"El año{txt} no es bisiesto")
